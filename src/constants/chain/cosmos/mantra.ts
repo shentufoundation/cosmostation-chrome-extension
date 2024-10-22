fc18098d11cd56879c159f38abbd8dd7e09d283e
Sihun Ahn <87967564+G-Gamja@users.noreply.github.com>
2024-10-22 17:20:49 +0900

    [APIS-446] Add MANTRA chain to Cosmos network (#391)

---
+++ src/constants/chain/cosmos/mantra.ts
@@ -0,0 +1,32 @@
+import { MINTSCAN_URL } from '~/constants/common';
+import mantraChainImg from '~/images/chainImgs/mantra.png';
+import mantraTokenImg from '~/images/symbols/om.png';
+import type { CosmosChain } from '~/types/chain';
+
+export const MANTRA: CosmosChain = {
+  id: '5b11ccac-d8a8-44af-937b-b423d1ddbbee',
+  line: 'COSMOS',
+  type: '',
+  chainId: 'mantra-1',
+  chainName: 'MANTRA',
+  restURL: 'https://lcd-mantra.cosmostation.io',
+  tokenImageURL: mantraTokenImg,
+  imageURL: mantraChainImg,
+  baseDenom: 'uom',
+  displayDenom: 'OM',
+  decimals: 6,
+  bip44: {
+    purpose: "44'",
+    coinType: "118'",
+    account: "0'",
+    change: '0',
+  },
+  bech32Prefix: { address: 'mantra' },
+  explorerURL: `${MINTSCAN_URL}/mantra`,
+  gasRate: {
+    tiny: '0.01',
+    low: '0.01',
+    average: '0.01',
+  },
+  gas: { send: '100000' },
+};
